b64c68e0a12feece80aff706fc556e9d2054bcb5
Cleber Medeiros Braz <mpontoc@hotmail.com>
2019-01-31 21:24:31 -0200

    updatede headless chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,18 +1,24 @@
 require 'rspec'
 require 'cucumber'
-require 'capybara'
-require 'selenium-webdriver'
-require 'capybara/cucumber'
+require 'selenium/webdriver'
+require 'capybara/dsl'
 
-#Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true) -------para firefox
 
-Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
-    #Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
+World Capybara::DSL
+BROWSER = ENV['BROWSER']
+
+Capybara.default_driver = :selenium_chrome
+
+if BROWSER.eql?('remote')
+  Capybara.register_driver :selenium_chrome do |app|
+    Capybara::Selenium::Driver.new(app,
+      :browser => :remote,
+      :desired_capabilities => :chrome,
+      :url => "http://www.google.com"
+    )
+  end
+else
+  Capybara.register_driver :selenium_chrome do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
 end
-
-    Capybara.configure do |config|
-            config.default_driver = :selenium
-            config.app_host = 'http://www.google.com'
-    end
-
