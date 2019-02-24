0842f059acede250d36d95652654f95d0ed4b53a
Cleber Medeiros Braz <mpontoc@hotmail.com>
2019-02-24 13:24:53 -0300

    Update and rename env to env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -20,5 +20,6 @@ if BROWSER.eql?('remote')
 else
   Capybara.register_driver :selenium_chrome do |app|
     Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    #Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
   end
 end
