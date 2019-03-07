5c182ae922fd875084883bd61ccc3d72c9d13ac4
Cleber Medeiros Braz <mpontoc@hotmail.com>
2019-03-07 16:50:15 -0300

    Update steps.rb

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,8 +1,8 @@
-Dado("que eu esteja no google") do
+ado("que eu esteja no google") do
   visit('http://www.google.com')
 end
 
-Quando("busco por selenium ruby") do
+Quando("busco por selenium ruby bindings") do
   fill_in('q', :with => 'selenium ruby').native.send_keys(:return)
 end
 
@@ -10,6 +10,7 @@ Então("acho as inforações sobre selenium ruby") do
   assert_text('ruby' , 'selenium')
 end
 
-Então("e bindings") do
+Então("bindings") do
   assert_text('bindings')
 end
+
