d1ef8695a892b9da6bfa1f22743053b2b44d09e7
Cleber Medeiros Braz <mpontoc@hotmail.com>
2019-04-16 11:11:06 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -12,3 +12,18 @@ Dir.mkdir('prints') unless Dir.exist?('prints')
 	page.save_screenshot("prints/#{sufix}-#{name}.png")
 	embed("prints/#{sufix}-#{name}.png", 'image/png', 'Screenshot')
 end
+
+at_exit do
+    time = Time.now.getutc
+    ReportBuilder.configure do |config|
+    config.junit_path = 'result'    
+    config.json_path = 'report.json'
+    config.report_path = 'cucumber_web_report'
+    config.report_types = [:html]
+    config.report_tabs = %w[Overview Features Scenarios Errors]
+    config.report_title = 'Cucumber Report Builder web automation test results'
+    config.compress_images = false
+    config.additional_info = { 'Project name' => 'Test', 'Platform' => 'Integration', 'Report generated' => time }
+    end
+    ReportBuilder.build_report
+end
