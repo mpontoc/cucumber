01b51121e5e55e9498017f18846ba03daf5d0ef9
Cleber Medeiros Braz <mpontoc@hotmail.com>
2019-04-16 14:13:48 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,16 +1,29 @@
-Before do
-	
-	Capybara.reset_sessions!
-end
-
 After do |scenario|
-Dir.mkdir('prints') unless Dir.exist?('prints')
+    add_screenshot(scenario)
 
-	sufix = ('error' if scenario.failed?) || 'success'
-	name = scenario.name.tr(' ', '_').downcase
+    if scenario.failed?
+        add_browser_logs
+    end
+ end
 
-	page.save_screenshot("prints/#{sufix}-#{name}.png")
-	embed("prints/#{sufix}-#{name}.png", 'image/png', 'Screenshot')
+ def add_screenshot(scenario)
+    nome_cenario = scenario.name.gsub(/[^A-Za-z0-9]/, '')
+    nome_cenario = nome_cenario.gsub(' ','_').downcase!
+    screenshot = "log/screenshots/#{nome_cenario}.png"
+    page.save_screenshot(screenshot)
+    embed(screenshot, 'image/png', 'Print maroto :)')
+ end
+
+ def add_browser_logs
+    time_now = Time.now
+    # Getting current URL
+    current_url = Capybara.current_url.to_s
+    # Gather browser logs
+    logs = page.driver.browser.manage.logs.get(:browser).map {|line| [line.level, line.message]}
+   # Remove warnings and info messages
+    logs.reject! { |line| ['WARNING', 'INFO'].include?(line.first) }
+    logs.any? == true
+    embed(time_now.strftime('%Y-%m-%d-%H-%M-%S' + "\n") + ( "Current URL: " + current_url + "\n") + logs.join("\n"), 'text/plain', 'BROWSER ERROR')
 end
 
 at_exit do
